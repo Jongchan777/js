3695ab659cb2214c1fb46ac378798d85b6204942
JC Kim <rlawhdcks12@gmail.com>
2023-04-29 11:19:37 +0900

    4'd commit

--- js_project/src/weather.js
+++ js_project/src/weather.js
@@ -4,15 +4,18 @@ const API_KEY = "095b92a0f21eb30ceb5014343f428dee";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in", lat, lng);
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
     });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
 }
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
